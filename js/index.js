6059b481df514f9982afbd9a013ef85ce016f0d6
Yan Velasco <yancarlosduartevelasco@gmail.com>
2023-09-05 15:47:51 -0300

    Finish

--- js/index.js
+++ js/index.js
@@ -13,11 +13,11 @@ function generateQRCode() {
 
     QRCodeIMG.style.visibility = "visible";
 
-    if ( QRCodeIMG.style.visibility = "visible") {
+    if ( QRCodeIMG.style.opacity = 1) {
         qrCodeBTN.innerHTML = "QR Code gerado abaixo:";
     }
 
-    QRCodeIMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=${QRCodeInputValue}`;
+    QRCodeIMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=${QRCodeInputValue}`;
 
     containerQRCode.classList.add("active")
 
@@ -31,5 +31,15 @@ qrCodeBTN.addEventListener("click", () => {
 QRCodeInput.addEventListener("keydown", (e) => {
     if (e.code === "Enter") {
         generateQRCode();
+    }
+});
+
+// Voltar ao estágio inicial da aplicação
+QRCodeInput.addEventListener("keyup", () => {
+    if (!QRCodeInput.value) {
+        QRCodeIMG.style.transition = "0.5s"; // Use "0.5s" instead of 0.5s
+        QRCodeIMG.style.opacity = 0;
+        containerQRCode.classList.remove("active");
+        qrCodeBTN.innerHTML = "Gerar QR Code";
     }
 });
